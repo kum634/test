6337942cfc5220f742ce4437ed9ef0a21712080f
kum634 <umechan517634@gmail.com>
2020-11-23 14:20:34 +0900

    最初のコミット

---
+++ test.php
@@ -0,0 +1,11 @@
+<!doctype html>
+<html>
+<head>
+<meta charshello worldet="utf-8">
+<title>hello world</title>
+<meta name="format-detection" content="telephone=no">
+</head>
+<body>
+  <h1>hello world</h1>
+</body>
+</html>
